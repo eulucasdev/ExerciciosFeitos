af95c1ec0adbfc39137f15c32bd849b7f1e514e8
eulucasdev <eulucasdev@gmail.com>
2024-02-22 22:03:00 -0300

    new exercises

---
+++ MUNDO1/ex006-Dobro, Triplo, Raiz Quadrada.py
@@ -0,0 +1,7 @@
+n = int(input("Entre com um número inteiro: "))
+d = n*2
+t = n*3
+r = n**(1/2)
+print(f"O dobro de {n} é {d}")
+print(f"O triplo de {n} é {t}")
+print(f"A raiz quadrada de {n} é {r:.2f}")
