5695e3b151f4c6de5b8535e0813535e698bebd0a
Lucas <eulucasdev@gmail.com>
2024-02-25 19:24:09 -0300

    Merge remote-tracking branch 'origin/main'

---
+++ MUNDO1/ex009-Tabuada.py
@@ -0,0 +1,14 @@
+n = int(input("Entre com um número inteiro: "))
+print(f"A tabuada de {n} é: ")
+print("x"*12)
+print(f"{n} x {1:>2} = {n*1}\n"
+      f"{n} x {2:>2} = {n*2}\n"
+      f"{n} x {3:>2} = {n*3}\n"
+      f"{n} x {4:>2} = {n*4}\n"
+      f"{n} x {5:>2} = {n*5}\n"
+      f"{n} x {6:>2} = {n*6}\n"
+      f"{n} x {7:>2} = {n*7}\n"
+      f"{n} x {8:>2} = {n*8}\n"
+      f"{n} x {9:>2} = {n*9}\n"
+      f"{n} x {10:>2} = {n*10}")
+print("x"*12)
---
+++ MUNDO1/ex010-Conversor de Moedas.py
@@ -0,0 +1,3 @@
+din = float(input("Quanto de dinheiro tem? "))
+dol = din / 5
+print(f"Com R${din:.2f} você conssegue comprar USD{dol:.2f}")
