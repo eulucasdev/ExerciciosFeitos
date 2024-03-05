7acca599463df33251339e4167216f61ffc2b867
eulucasdev <eulucasdev@gmail.com>
2024-03-04 21:24:37 -0300

    Manipulando Textos

---
+++ MUNDO1/02-Módulos em Python e Manipulando Texto/ex022-Analisador de Textos.py
@@ -0,0 +1,4 @@
+nome = input("Digite o nome completo: ")
+lnome = nome.replace(" ", "")
+print(f"Seu nome com todas as letras maiusculas: {nome.upper()} ")
+print(f"Seu nome com todas as letras minusculas: {nome.lower()} ")
