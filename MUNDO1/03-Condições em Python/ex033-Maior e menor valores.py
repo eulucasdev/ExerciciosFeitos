1a7bb9b48580d07379786a5e7ed51c60545415f0
eulucasdev <eulucasdev@gmail.com>
2024-03-19 22:29:57 -0300

    if / else

---
+++ MUNDO1/03-Condições em Python/ex033-Maior e menor valores.py
@@ -0,0 +1,15 @@
+n1 = int(input("Entre com primeiro número: "))
+n2 = int(input("Entre com segundo número: "))
+n3 = int(input("Entre com terceiro número: "))
+menor = n1
+maior = n1
+if n2 < n1 and n2 < n3:
+    menor = n2
+if n3 < n1 and n3 < n2:
+    menor = n3
+if n2 > n1 and n2 > n3:
+    maior = n2
+if n3 > n1 and n3 > n2:
+    maior = n3
+print(f"O Maior número é {maior} e o menor número é {menor}")
+
