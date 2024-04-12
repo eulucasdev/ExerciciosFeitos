414fd4caaf98e447dfbd965cff26e1b34ee8b3d8
eulucasdev <eulucasdev@gmail.com>
2024-04-12 19:07:49 -0300

    for

---
+++ MUNDO - 02/02-Rapetições em Python (For)/ex047-Contagem de pares.py
@@ -0,0 +1,10 @@
+n = int(input("quer ver os pares até qual número: "))
+quebra = 0
+for c in range(1, n+1):
+    if c >= quebra or c == 50:
+        print("\n")
+        quebra += 50
+    if c % 2 == 0:
+        print(c, end=" ")
+print("FIM!!!")
+print(quebra)
