3bc0e94952dfc7a4ded2847acdb7e658bd7104c3
Lucas <eulucasdev@gmail.com>
2024-04-13 20:01:13 -0300

    Merge remote-tracking branch 'origin/main'

---
+++ MUNDO - 02/01-Condições em Python/ex042-Analisando Triângulos v2.0.py
@@ -0,0 +1,13 @@
+n1 = float(input("Primeira reta: "))
+n2 = float(input("Segunda reta: "))
+n3 = float(input("Terceira reta: "))
+
+if n1 + n2 > n3 and n2 + n3 > n1 and n3 + n1 > n2:
+    r1 = "Esse triângulo e"
+    if n1 == n2 == n3:
+        r2 = "EQUILÁTERO: todos os lados iguais"
+    elif n1 == n2 != n3 or n2 == n3 != n1 or n3 == n1 != n2:
+        r2 = "ISÓSCELES: dois lados iguais, um diferente"
+    else:
+        r2 = "ESCALENO: todos os lados diferentes"
+    print(r1, r2)
