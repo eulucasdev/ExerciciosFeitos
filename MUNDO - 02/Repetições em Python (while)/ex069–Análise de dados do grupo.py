ab1344bb847cc4cb492cf0ad6467a4a5ef27e736
eulucasdev <eulucasdev@gmail.com>
2024-05-24 12:02:52 -0300

    (while)

--- MUNDO - 02/Repetições em Python (while)/ex069–Análise de dados do grupo.py
+++ MUNDO - 02/Repetições em Python (while)/ex069–Análise de dados do grupo.py
@@ -6,6 +6,7 @@ while True:
         sexo = str(input("Sexo [F/M] ")).strip().lower()[0]
     while sn not in "sn":
         sn = input("Cadastrar outra pessoas? ")
+    print("=-"*15)
     if idade >= 18:
         t18 += 1
     if sexo == "m":
@@ -14,6 +15,6 @@ while True:
         m20 += 1
     if sn == "n":
         break
-print(f"Total de homens: {th}\n"
+print(f"\nTotal de homens: {th}\n"
       f"Total de pessoas maiores de 18: {t18}\n"
       f"Total de Mulheres maiores de 20 anos: {m20}")
