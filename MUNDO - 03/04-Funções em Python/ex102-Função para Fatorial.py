bf18ccc2b2bb271d2a228435c5e2bdd4cdd4ec1e
eulucasdev <eulucasdev@gmail.com>
2024-09-17 22:05:00 -0300

    (FUNÇÕES)

---
+++ MUNDO - 03/04-Funções em Python/ex102-Função para Fatorial.py
@@ -0,0 +1,19 @@
+def fator(f, show=False):
+    """
+fator(n, show=False)
+    -> Calcula o Fatorial de um número
+    :param f: O número a ser calculado.
+    :param show: (opcional) Mostrar ou não a conta
+    :return: O valor do Fatorial de um número n.
+    """
+    for c in range(f-1, 0, -1):
+        f *= c
+        if show:
+            if c>1:
+                print(c, end=" x ")
+            else:
+                print(c, end=" = ")
+    return f
+
+
+print(fator(10, show=True))
