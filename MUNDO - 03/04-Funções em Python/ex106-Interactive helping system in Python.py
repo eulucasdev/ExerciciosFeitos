5a362138ae079a16a298ac28caed8757f31471cf
eulucasdev <eulucasdev@gmail.com>
2024-10-23 15:28:29 -0300

    (FUNÇÕES)

---
+++ MUNDO - 03/04-Funções em Python/ex106-Interactive helping system in Python.py
@@ -0,0 +1,24 @@
+def title(msg):
+    tam = len(msg) + 4
+    print("~"*tam)
+    print(f"{msg:^{tam}}")
+    print("~"*tam)
+
+
+def ajuda():
+    while True:
+        print("\033[01;30;42m", end="")
+        title('SISTEMA DE AJUDA PyHELP')
+        msg = input("\033[mQual comando quer saber?").lower().strip()
+        print("\033[01;31;44m", end="")
+        if msg != "fim":
+            title(f"Acessando o manual do comando '{msg}' ")
+            print("\033[m", end="")
+            print("\033[1;30;47m")
+            help(msg)
+        if msg == "fim":
+            break
+    print("\033[m\033[01;31;43mPROGRAMA ENCERRADO!")
+
+
+ajuda()
