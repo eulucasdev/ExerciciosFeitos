362a1d9e5a46e103796ff2ccdb03812451627e5d
eulucasdev <eulucasdev@gmail.com>
2024-11-07 22:50:34 -0300

    (MODULOS)

---
+++ MUNDO - 03/05-Modularização em Python/ex110-Reduzindo ainda mais seu programa.py
@@ -0,0 +1,4 @@
+from uteis.numeros import moeda
+
+p = float(input("Entre com o valor: "))
+moeda.resumo(p, 20, 12)
--- MUNDO - 03/05-Modularização em Python/uteis/numeros/moeda.py
+++ MUNDO - 03/05-Modularização em Python/uteis/numeros/moeda.py
@@ -22,3 +22,16 @@ def metade(valor=0, formata=False):
 # ----->  ex108  <-----
 def real(valor=0, moeda="R$"):
     return f'{moeda} {valor:>.2f}'.replace('.', ',')
+
+
+def resumo(v, up=0, dow=0):
+    print("="*35)
+    print(f"{"RESUMO DE VALOR":^35}")
+    print("="*35)
+    print(f"Preço analisado:{real(v):<18}")
+    print(f"Dobro do preço:{dobro(v, True):<19}")
+    print(f"Metade do preço:{metade(v, True):<18}")
+    print(f"{up}% de aumento:{aumentar(v, up, True):<19}")
+    print(f"{dow}% de redução:{diminuir(v, dow, True):<19}")
+    print("="*35)
+
